7bfd0755206dfb27a73f473eaaff4d24509a0734
Jolene115 <jteo0043@student.monash.edu>
2025-05-16 01:34:27 +1000

    change port to 5050 as 5000 is already in use and busy

--- backend/app.py
+++ backend/app.py
@@ -109,5 +109,5 @@ def health_check():
 
 if __name__ == '__main__':
     # For local development
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host='0.0.0.0', port=port, debug=True)
